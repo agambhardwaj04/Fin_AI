5d83adb40ac84202389fd1677bbfa32262f32ad6
Agam <agam@gmail.com>
2026-03-17 15:11:16 +0530

    features_added_and_documentation

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,7 +2,13 @@ import yfinance as yf
 import pandas as pd
 
 def load_stock_data(ticker):
+  # Downloading the data through yfinance 
   data = yf.download(ticker, start="2019-01-01")
+
+
+  # Selecting only columns close and volume as: 
+  # 1.) Volume will tell number of units traded during a time period and
+  # 2.) Close will tell the final price of an asset at the end of a time period
   data = data[['Close','Volume']]
   data.dropna(inplace=True)
   return data
--- src/features.py
+++ src/features.py
@@ -1,4 +1,16 @@
 import numpy as np
 def add_features(data):
+
+  # Adding Features and Target 
   data["MA50"] = data["Close"].rolling(50).mean()
+
   data["MA200"] = data["Close"].rolling(200).mean()
+
+  data["Return"] = data["Close"].pct_change()
+
+  data["Volatility"] = data["Return"].rolling(20).std()
+
+  data["Target"] = np.where(data["Close"].shift(-1) > data["Close"],1 , 0)
+  
+  return data
+
